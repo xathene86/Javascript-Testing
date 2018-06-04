18f942a549d8788d55629516de50d154692a2cd5
xathene86 <38136887+xathene86@users.noreply.github.com>
2018-06-04 17:51:02 -0500

    Messing around with the dom
    Messing around with the dom

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
-var text = document.getElementById("HeaderOne");
-text.innerHTML = "Noice Again";
+var text = document.getElementById("text");
+var button = document.getElementById("custom-button");
+
+button.addEventListener("mouseover",onHover);
+button.addEventListener("click" ,onClick);
+button.addEventListener("mouseout",onHoverOut)
+function onClick(){
+text.style.setProperty("color","red")
+}
+function onHover(){
+    button.style.setProperty("background-color","blue");
+}
+function onHoverOut(){
+    button.style.setProperty("background-color","red");
+}
